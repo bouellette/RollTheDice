bbfe0c8804b63a7fe8622379773caecd4ab812a4
Bob Ouellette <bouellette@microdesk.com>
2016-06-06 09:13:10 -0400

    Initial commit to local repository

---
+++ Dice/DiceCup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollTheDice
+{
+    public class DiceCup
+    {
+        public DiceCup()
+        {
+            RandomNumberGenerator = new Random();
+        }
+
+        public Random RandomNumberGenerator { get; set; }
+
+        public List<int> Roll(int numberOfDice)
+        {
+            List<int> dice = new List<int>();
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                dice.Add(RandomNumberGenerator.Next(1, 7));
+            }
+            return dice;
+        }
+    }
+}
---
+++ RollTheDiceConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RollTheDice;
+
+namespace RollTheDiceConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceCup cup = new DiceCup();
+
+            // Role the dice 100 times...
+            for (int i = 0; i < 100; i++)
+            {
+                List<int> results = cup.Roll(2);
+
+                int total = 0;
+                foreach (int r in results)
+                {
+                    total += r;
+                }
+
+                Console.WriteLine(String.Format("Roll result = {0}", total));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
